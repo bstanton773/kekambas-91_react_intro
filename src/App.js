ee227998652faca7b0bf4c781540cdb516cda7a3
Brian <bstanton773@gmail.com>
2022-06-21 15:59:58 -0500

    Move count state from Button components to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React, { useState } from 'react';
 import Button from './components/Button';
 import Nav from './components/Nav';
 
 
 function App(props) {
-  console.log('Hello this function is running!')
+  const [count, setCount] = useState(0)
+  const handleButtonClick = (step) => {
+    setCount(count + step)
+  }
+  const myButtonSteps = [1, 10, 100, 1000]
   return (
     <div id="fromApp">
         <Nav brand="Kekambas React"/>
         <div className='container'>
           <h1>Hello World</h1>
-          <Button step={1} />
-          <Button step={10} />
-          <Button step={100} />
-          <Button step={1000} />
+          <h4>Current Value: {count}</h4>
+          {myButtonSteps.map((step, i) => <Button step={step} handleClick={handleButtonClick} key={i} />)}
         </div>
     </div>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Button(props) {
-    const [count, setCount] = useState(0)
+    
     return (
-        <button className='btn btn-primary w-100' onClick={() => setCount(count + props.step)}>Step: {props.step} - Current: {count} </button>
+        <button className='btn btn-primary w-100' onClick={() => props.handleClick(props.step)}>Step: {props.step} </button>
     )
 }
