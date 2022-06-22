3a43a51c21bb5433eaef927d1a0fb05a79bf0d9d
Brian <bstanton773@gmail.com>
2022-06-22 10:57:31 -0500

    add useEffect to App component to get racer data and log out to console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './components/Button';
 import Nav from './components/Nav';
 
 
 function App(props) {
   const [count, setCount] = useState(0)
+  const [racers, setRacers] = useState([])
+  useEffect(() => {
+    console.log('App rendered!')
+    fetch('https://ergast.com/api/f1/2021/10/driverStandings.json')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  })
   const handleButtonClick = (step) => {
     setCount(count + step)
   }
