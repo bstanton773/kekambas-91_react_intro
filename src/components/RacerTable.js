4fdf649f29fe0aca0ca1583486dd37f1fd677acb
Brian <bstanton773@gmail.com>
2022-06-22 13:54:17 -0500

    Convert RacerTable Component from Function to Class

--- src/components/RacerTable.js
+++ src/components/RacerTable.js
@@ -2,22 +2,46 @@ import React from 'react'
 import RacerForm from './RacerForm';
 import RacerRow from './RacerRow';
 
-export default function RacerTable(props) {
-    let headers = ['#', 'First', 'Last', 'Points', 'Wins', 'Nationality', 'Constructor'];
-    return (
-        <>
-            <h1 className='text-center mt-5'>Driver Standings</h1>
-            <RacerForm handleFormSubmit={props.handleFormSubmit} />
-            <table className='table table-primary table-striped'>
-                <thead>
-                    <tr>
-                        {headers.map((elem,idx) => <th key={idx}>{elem}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.racers.map((racer, idx) => <RacerRow racer={racer} key={idx} />)}
-                </tbody>
-            </table>
-        </>
-    )
+export default class RacerTable extends React.Component{
+    render(){
+        let headers = ['#', 'First', 'Last', 'Points', 'Wins', 'Nationality', 'Constructor'];
+        return (
+            <>
+                <h1 className='text-center mt-5'>Driver Standings</h1>
+                <RacerForm handleFormSubmit={this.props.handleFormSubmit} />
+                <table className='table table-primary table-striped'>
+                    <thead>
+                        <tr>
+                            {headers.map((elem,idx) => <th key={idx}>{elem}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.racers.map((racer, idx) => <RacerRow racer={racer} key={idx} />)}
+                    </tbody>
+                </table>
+            </>
+        )
+    }
 }
+
+
+// OLD FUNCTION COMPONENT - DOES SAME THING AS CLASS COMPONENT ABOVE
+// export default function RacerTable(props) {
+//     let headers = ['#', 'First', 'Last', 'Points', 'Wins', 'Nationality', 'Constructor'];
+//     return (
+//         <>
+//             <h1 className='text-center mt-5'>Driver Standings</h1>
+//             <RacerForm handleFormSubmit={props.handleFormSubmit} />
+//             <table className='table table-primary table-striped'>
+//                 <thead>
+//                     <tr>
+//                         {headers.map((elem,idx) => <th key={idx}>{elem}</th>)}
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {props.racers.map((racer, idx) => <RacerRow racer={racer} key={idx} />)}
+//                 </tbody>
+//             </table>
+//         </>
+//     )
+// }
