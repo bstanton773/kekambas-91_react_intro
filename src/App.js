1129447b50d9a9e2591d4f9be521b9f158fabe2c
Brian <bstanton773@gmail.com>
2022-06-22 11:11:05 -0500

    Add RacerTable component to App and fix tabbing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Button from './components/Button';
 import Nav from './components/Nav';
+import RacerTable from './components/RacerTable';
 
 
 function App(props) {
-  const [count, setCount] = useState(0)
-  const [racers, setRacers] = useState([])
-  useEffect(() => {
-    console.log('App rendered!')
-    fetch('https://ergast.com/api/f1/2021/10/driverStandings.json')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-        setRacers(racerStandings)
-      })
-  }, [])
-  const handleButtonClick = (step) => {
-    setCount(count + step)
-  }
-  const myButtonSteps = [1, 10, 100, 1000]
-  return (
-    <div id="fromApp">
-        <Nav brand="Kekambas React"/>
-        <div className='container'>
-          <h1>Hello World</h1>
-          <h4>Current Value: {count}</h4>
-          {myButtonSteps.map((step, i) => <Button step={step} handleClick={handleButtonClick} key={i} />)}
+    const [count, setCount] = useState(0)
+    const [racers, setRacers] = useState([])
+    useEffect(() => {
+        console.log('App rendered!')
+        fetch('https://ergast.com/api/f1/2021/10/driverStandings.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            setRacers(racerStandings)
+        })
+    }, [])
+    const handleButtonClick = (step) => {
+        setCount(count + step)
+    }
+    const myButtonSteps = [1, 10, 100, 1000]
+    return (
+        <div id="fromApp">
+            <Nav brand="Kekambas React"/>
+            <div className='container'>
+            <h1>Hello World</h1>
+            <h4>Current Value: {count}</h4>
+            {myButtonSteps.map((step, i) => <Button step={step} handleClick={handleButtonClick} key={i} />)}
+            <RacerTable />
+            </div>
         </div>
-    </div>
     );
 }
 
---
+++ src/components/RacerTable.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RacerTable() {
+    return (
+        <h1 className='text-center mt-5'>Driver Standings</h1>
+    )
+}
