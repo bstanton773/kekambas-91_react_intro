728b4a42dd7689e46aa52cf5983483b7da042439
Brian <bstanton773@gmail.com>
2022-06-23 11:18:04 -0500

    Get Kekambas from Button Click instead of automatically

--- src/views/KekambasFun.js
+++ src/views/KekambasFun.js
@@ -3,15 +3,22 @@ import React, { useState, useEffect } from 'react';
 export default function KekambasFun(props) {
     const [kekambas, setKekambas] = useState([])
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     fetch('https://kekambas-bs.herokuapp.com/kekambas')
+    //         .then(res => res.json())
+    //         .then(data => setKekambas(data))
+    // }, [])
+
+    const handleClick = () => {
         fetch('https://kekambas-bs.herokuapp.com/kekambas')
             .then(res => res.json())
             .then(data => setKekambas(data))
-    }, [])
+    }
 
     return (
         <>
             <h1 className='text-center'>Kekambas Function</h1>
+            <button className='btn btn-success' onClick={handleClick}>Get Kekambas</button>
             <ul>
                 {kekambas.map(k => <li key={k.id}>{k.first_name} {k.last_name}</li>)}
             </ul>
