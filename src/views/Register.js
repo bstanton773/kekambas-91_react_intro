ca688b528a263ccf68f89ee9eae476cb6345d81e
Brian <bstanton773@gmail.com>
2022-06-24 14:43:34 -0500

    Add register form and handle submit

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+
+    let navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(e);
+
+        // Check that the passwords match
+        let password = e.target.password.value;
+        let confirmPass = e.target.confirmPass.value;
+        if(password !== confirmPass){
+            console.log('Passwords do not match')
+        } else {
+            // Set up the request to Flask API
+            let myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json')
+
+            let data = JSON.stringify({
+                username: e.target.username.value,
+                email: e.target.email.value,
+                password: password
+            })
+
+            fetch('http://localhost:5000/api/users', {
+                method: 'POST',
+                headers: myHeaders,
+                body: data
+            }).then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    navigate('/')
+                })
+        }
+
+    }
+
     return (
-        <div>Register</div>
+        <>
+        <h4 className='text-center'>Register</h4>
+        <form onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor='email'>Email</label>
+                <input type='text' className='form-control' placeholder='Enter Email' name='email' />
+
+                <label htmlFor='username'>Username</label>
+                <input type='text' className='form-control' placeholder='Enter Username' name='username' />
+
+                <label htmlFor='password'>Password</label>
+                <input type='password' className='form-control' placeholder='Enter Password' name='password' />
+
+                <label htmlFor='confirmPass'>Confirm Password</label>
+                <input type='password' className='form-control' placeholder='Enter Password Again' name='confirmPass' />
+
+                <input type='submit' className='btn btn-primary w-100 mt-3' value='Register' />
+
+            </div>
+        </form>
+        </>
     )
 }
